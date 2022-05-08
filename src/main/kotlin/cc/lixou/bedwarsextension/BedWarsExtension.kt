ae603c54e6362e535d4dab77b69bf5a76ad6f745
DasLixou <einlixou@gmail.com>
2022-05-08 12:53:09 +0200

    🧪 put game joining in test class

--- src/main/kotlin/cc/lixou/bedwarsextension/BedWarsExtension.kt
+++ src/main/kotlin/cc/lixou/bedwarsextension/BedWarsExtension.kt
@@ -2,18 +2,12 @@ package cc.lixou.bedwarsextension
 
 import cc.lixou.bedwarsextension.game.BedWarsGame
 import cc.lixou.stracciatella.game.GameManager
-import net.minestom.server.event.player.PlayerLoginEvent
 import net.minestom.server.extensions.Extension
-import world.cepi.kstom.Manager
-import world.cepi.kstom.event.listenOnly
 
 class BedWarsExtension : Extension() {
 
     override fun initialize() {
         GameManager.registerGame<BedWarsGame>()
-        Manager.globalEvent.listenOnly<PlayerLoginEvent> {
-            GameManager.joinGame<BedWarsGame>(arrayOf(this.player))
-        }
         println("[BedWars] Initialized successfully")
     }
 
--- src/test/kotlin/TestServer.kt
+++ src/test/kotlin/TestServer.kt
@@ -1,5 +1,13 @@
+import cc.lixou.bedwarsextension.game.BedWarsGame
 import cc.lixou.stracciatella.Stracciatella
+import cc.lixou.stracciatella.game.GameManager
+import net.minestom.server.event.player.PlayerLoginEvent
+import world.cepi.kstom.Manager
+import world.cepi.kstom.event.listenOnly
 
 fun main() {
     Stracciatella().start()
+    Manager.globalEvent.listenOnly<PlayerLoginEvent> {
+        GameManager.joinGame<BedWarsGame>(arrayOf(this.player))
+    }
 }

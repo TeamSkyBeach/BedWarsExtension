07e89491b4be2d120cad3f55897e247e43d78bac
DasLixou <einlixou@gmail.com>
2022-05-08 12:48:42 +0200

    🕹️ Added BedWarsGame class

---
+++ src/main/kotlin/cc/lixou/bedwarsextension/game/BedWarsGame.kt
@@ -0,0 +1,24 @@
+package cc.lixou.bedwarsextension.game
+
+import cc.lixou.stracciatella.game.Game
+import net.minestom.server.entity.Player
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
+
+class BedWarsGame : Game() {
+
+    companion object {
+        val MAX_PLAYERS = 8
+    }
+
+    override fun canJoin(players: Array<Player>): Boolean = (this.players.size + players.size) >= MAX_PLAYERS
+
+    override fun onJoin(player: Player) {
+        player.inventory.setItemStack(0, ItemStack.of(Material.WOODEN_SWORD))
+    }
+
+    override fun onLeave(player: Player) {
+        player.sendMessage("bye bye")
+    }
+
+}

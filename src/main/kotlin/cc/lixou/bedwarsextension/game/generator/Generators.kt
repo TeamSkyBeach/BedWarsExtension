458ea2086317d65ed0e9498b5655fd0b510ab25e
DasLixou <einlixou@gmail.com>
2022-05-17 20:57:56 +0200

    💎 Added new Generators

--- src/main/kotlin/cc/lixou/bedwarsextension/game/generator/Generators.kt
+++ src/main/kotlin/cc/lixou/bedwarsextension/game/generator/Generators.kt
@@ -7,4 +7,8 @@ object Generators {
 
     val DIAMOND: GeneratorFabric = GeneratorFabric(Material.DIAMOND_BLOCK, Material.DIAMOND, {lvl -> "<gradient:dark_aqua:aqua><bold>Diamond $lvl".asMini()}, 20)
 
+    val EMERALD: GeneratorFabric = GeneratorFabric(Material.EMERALD_BLOCK, Material.EMERALD, {lvl -> "<gradient:dark_green:green><bold>Emerald $lvl".asMini()}, 40)
+
+    val LAPIS: GeneratorFabric = GeneratorFabric(Material.LAPIS_BLOCK, Material.LAPIS_LAZULI, {lvl -> "<gradient:dark_blue:blue><bold>Lapis $lvl".asMini()}, 10)
+
 }
